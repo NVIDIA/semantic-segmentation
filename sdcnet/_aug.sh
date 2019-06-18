4ddef0037f71bdaa160bea97b49be8e9a5089b23
Zhu <yzaws@38f9d370d5fb.ant.amazon.com>
2019-06-18 15:18:45 -0700

    add sdc augmentation

---
+++ sdcnet/_aug.sh
@@ -0,0 +1,9 @@
+python sdc_aug.py --propagate 5 --vis \
+		--pretrained ../pretrained_models/sdc_cityscapes_vrec.pth.tar \
+		--flownet2_checkpoint ../pretrained_models/FlowNet2_checkpoint.pth.tar \
+		--source_dir /home/ubuntu/yizhu/data/Cityscapes \
+		--target_dir /home/ubuntu/yizhu/data/Cityscapes/cs_aug
+		
+
+
+		
