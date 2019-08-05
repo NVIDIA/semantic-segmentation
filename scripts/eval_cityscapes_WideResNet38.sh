40acd19cbd32896729d48166e19955d0b5c078ba
bryanzhu <yizhu59@gmail.com>
2019-08-05 16:11:38 -0700

    update readme

---
+++ scripts/eval_cityscapes_WideResNet38.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "Running inference on" ${1}
+echo "Saving Results :" ${2}
+PYTHONPATH=$PWD:$PYTHONPATH python3 eval.py \
+	--dataset cityscapes \
+    --arch network.deepv3.DeepWV3Plus \
+    --inference_mode sliding \
+    --scales 1.0 \
+    --split val \
+    --cv_split 0 \
+    --dump_images \
+    --ckpt_path ${2} \
+    --snapshot ${1}
---
+++ scripts/train_mapillary_SEResNeXt50.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+    # Example on Mapillary
+     python -m torch.distributed.launch --nproc_per_node=8 train.py \
+        --dataset mapillary \
+        --arch network.deepv3.DeepSRNX50V3PlusD_m1 \
+        --class_uniform_pct 0.5 \
+        --class_uniform_tile 1024 \
+        --syncbn \
+        --sgd \
+        --lr 2e-2 \
+        --lr_schedule poly \
+        --poly_exp 1.0 \
+        --crop_size 896 \
+        --scale_min 0.5 \
+        --scale_max 2.0 \
+        --color_aug 0.25 \
+        --gblur \
+        --max_epoch 175 \
+        --img_wt_loss \
+        --wt_bound 6.0 \
+        --bs_mult 2 \
+        --apex \
+        --exp mapillary_pretrain \
+        --ckpt ./logs/ \
+        --tb_path ./logs/ 
